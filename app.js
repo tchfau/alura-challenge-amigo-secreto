55f972de0b339697748baa39ddd1c10d62f63b01
Ignacio Martin <tchfau@gmail.com>
2025-01-19 15:25:25 -0300

    Crea el array para almacenar los nombres, declara e inicializa variables globales

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Crea el array para almacenar los nombres, declara e inicializa variables globales
+let amigos = [];
+let elementoAmigo = document.getElementById('amigo');
+let elementoListaAmigos = document.getElementById('listaAmigos');
+let elementoListaResultados = document.getElementById('resultado'); 
+
