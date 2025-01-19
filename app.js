64e96811f88187bc1eb50dd2b6af85dbfd1ddb0c
Ignacio Martin <tchfau@gmail.com>
2025-01-19 15:31:37 -0300

    Implementa una función para mostrar los resultados

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // Crea el array para almacenar los nombres, declara e inicializa variables globales
 let amigos = [];
+let amigoSorteado = "";
 let elementoAmigo = document.getElementById('amigo');
 let elementoListaAmigos = document.getElementById('listaAmigos');
 let elementoListaResultados = document.getElementById('resultado'); 
@@ -32,4 +33,31 @@ function actualizarListaAmigos(amigos) {
     }
     console.log(nuevoElementoLi);
     elementoListaAmigos.append(nuevoElementoLi);
+}
+
+// Implementa una función para sortear los amigos
+function sortearAmigo() {
+    let indiceSorteado;
+    while (amigos.length == 0) {
+        alert("No hay amigos ingresados");
+        break;
+    }
+
+    for (let i = 0; i < amigos.length; i++) {
+        indiceSorteado = Math.floor(Math.random() * (i +1));
+    }
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == indiceSorteado) {
+            amigoSorteado = amigos[i];
+        }
+    }
+
+    mostrarResultados(amigoSorteado);
+}
+
+// Implementa una función para mostrar los resultados
+function mostrarResultados(amigoSorteado) {
+    lista.innerHTML = "";
+    resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`;
 }
